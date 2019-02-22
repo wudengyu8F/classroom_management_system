60ce696756dcb8c81bc44a450c0aecae01497063
01383849 <dengyu.wu@sfmail.sf-express.com>
2019-02-22 18:29:09 +0800

    shiro初步搭建

---
+++ classroom_features/src/main/java/com/yizhuoyang/classroomfeatures/shiro/ShiroConfig.java
@@ -0,0 +1,52 @@
+package com.yizhuoyang.classroomfeatures.shiro;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+
+    @Value("${}")
+    private String host;
+
+    @Value("${}")
+    private int port;
+
+    @Value("${}")
+    private int timeout;
+
+    @Value("${}")
+    private String password;
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+        System.out.println("ShiroConfiguration.shirFilter()");
+        System.out.println("ShiroConfiguration.shirFilter()");
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+        //注意过滤器配置顺序 不能颠倒
+        //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了，登出后跳转配置的loginUrl
+        filterChainDefinitionMap.put("/logout", "logout");
+        // 配置不会被拦截的链接 顺序判断
+        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/ajaxLogin", "anon");
+        filterChainDefinitionMap.put("/login", "anon");
+        filterChainDefinitionMap.put("/**", "authc");
+        //配置shiro默认登录界面地址，前后端分离中登录界面跳转应由前端路由控制，后台仅返回json数据
+        shiroFilterFactoryBean.setLoginUrl("/unauth");
+        // 登录成功后要跳转的链接
+//        shiroFilterFactoryBean.setSuccessUrl("/index");
+        //未授权界面;
+//        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+}
